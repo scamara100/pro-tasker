ec14a7d84c61263d50db50afba4858cf0fadea82
Sekouba CAMARA <sekoubacamara100@gmail.com>
2026-04-02 13:18:39 -0400

    added login page

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,5 +1,68 @@
+import { useState } from "react";
+import { userClient} from '../clients/api.js'
+import { useUser } from "../context/useUser.js";
+import { useNavigate } from "react-router-dom";
+
+
 function Login() {
-  return <div>Login Page</div>;
+
+  // bring the setter function for the context
+  const {setUser} = useUser()
+
+  const navigate = useNavigate()
+  
+  const [form, setForm] = useState({
+    email: '',
+    password: ''
+  })
+
+  function handleChange(e){
+    setForm({
+      ...form,
+      [e.target.name]: e.target.value
+    })
+  }
+
+  async function handleSubmit(e){
+      e.preventDefault()
+
+      console.log(form)
+
+      try{
+        // send the form data to our backend
+        const response = await userClient.post('/login', form)
+        console.log(response.data)
+
+        // take the token and stored it locally
+        localStorage.setItem('token', response.data.token)
+
+        // save some user data in our state
+        setUser(response.data.user)
+
+        // take the user to the different pages 
+        navigate('/feed')
+        
+      } catch(err){
+        console.log(err)
+            alert(err.message)
+      }
+
+    }
+  return (
+    <div>
+    <h1>Login Page</h1>
+    <form onSubmit={handleSubmit}>
+    
+      <label htmlFor="email">Email</label>
+      <input onChange={handleChange} value={form.email} id="email" name="email" type="email" />
+
+      <label htmlFor="password">Password</label>
+      <input onChange={handleChange} value={form.password} id="password" name="password" type="password" />
+
+    <button>Login</button>
+    </form>
+    </div>
+)
 }
 
 export default Login;
