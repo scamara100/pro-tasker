e24f7c6509a85d9685c14a49e1fb844e800be4b9
Sekouba CAMARA <sekoubacamara100@gmail.com>
2026-04-02 17:22:45 -0400

    added an interceptor

--- frontend/src/clients/api.js
+++ frontend/src/clients/api.js
@@ -1,15 +1,18 @@
 import axios from 'axios'
 
-const token = localStorage.getItem('token')
-console.log(token)
-
 export const userClient = axios.create({
     baseURL: 'http://localhost:8080/api/users'
 })
 
 export const projectClient = axios.create({
-    baseURL: 'http://localhost:8080/api/projects',
-    headers: {
-        Authorization: `Bearer ${token}`
+    baseURL: 'http://localhost:8080/api/projects'
+})
+
+projectClient.interceptors.request.use((req) =>{
+    const token = localStorage.getItem('token')
+
+    if(token){
+        req.headers.Authorization =  `Bearer ${token}`
     }
+    return req;
 })
