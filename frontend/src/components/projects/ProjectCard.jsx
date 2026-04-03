649bdcbfcfe02a5b1624a31d07b197893a5f865e
Sekouba CAMARA <sekoubacamara100@gmail.com>
2026-04-03 19:01:14 -0400

    Implemented ProjectPage and ProjectCard

--- frontend/src/components/projects/ProjectCard.jsx
+++ frontend/src/components/projects/ProjectCard.jsx
@@ -1,12 +1,14 @@
+import { useNavigate } from "react-router-dom";
 function ProjectCard({ project}){
+    const navigate = useNavigate();
+    const date = new Date(project.createdAt)
     return (
-        <div>
-            <div key={project._id}>
-            <div>{new Date(project.createdAt).toLocaleDateString()}  {new Date(project.createdAt).toLocaleTimeString()}</div>
-            <h3>{project.name}</h3>
-            <p>{project.description}</p>
-            </div>
-      </div>
+       
+    <div style={{border: "1px solid #ccc, padding: '10px", margin:  "10px", cursor: "pointer"}} onClick={() => navigate(`/projects/${project._id}`)}>
+        <div>{date.toLocaleDateString()}  {date.toLocaleTimeString()}</div>
+        <h3>{project.name}</h3>
+        <p>{project.description}</p>
+    </div>
     )
 }
 
--- frontend/src/pages/ProjectPage.jsx
+++ frontend/src/pages/ProjectPage.jsx
@@ -1,5 +1,43 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { projectClient } from "../clients/api";
+import {TaskCard} from "../components/tasks/TaskCard";
+
 function ProjectPage() {
-  return <div>Project Page</div>;
+  const { id } = useParams();
+
+  const [project, setProject] = useState(null);
+  const [tasks, setTasks] = useState([]);
+
+  useEffect(() => {
+    async function fetchProject() {
+      try {
+        const { data } = await projectClient.get(`/projects/${id}`);
+        setProject(data.project);
+        setTasks(data.tasks);
+      } catch (err) {
+        console.log(err);
+      }
+    }
+
+    fetchProject();
+  }, [id]);
+
+  if (!project) return <h2>Loading...</h2>;
+  return (
+    <div>
+      <h1>Project Page</h1>
+
+      <h2>{project.name}</h2>
+      <p>{project.description}</p>
+
+      <h3>Tasks</h3>
+
+      {tasks.map((task) => (
+        <TaskCard key={task._id} task={task} />
+      ))}
+    </div>
+  );
 }
 
 export default ProjectPage;
