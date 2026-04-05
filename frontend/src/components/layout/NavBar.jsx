4c4a83a40837aa1af89dffdac2335df080d6814d
Sekouba CAMARA <sekoubacamara100@gmail.com>
2026-04-05 19:22:09 -0400

    added styles and UI responsive

--- frontend/src/components/layout/NavBar.jsx
+++ frontend/src/components/layout/NavBar.jsx
@@ -5,30 +5,35 @@ function Navbar() {
   // bring in user info context
   const { user, logout } = useUser();
   return (
-    <nav>
-      <ul>
-        {user ? (
-          <>
-            <li>
-              <Link to="/">Dashboard</Link>
-            </li>
-            <li onClick={logout}>
-              <Link to="/login">Logout</Link>
-            </li>
-          </>
-        ) : (
-          <>
-            <li>
-              <Link to="/login">Login</Link>
-            </li>
-            <li>
-              <Link to="/register">Register</Link>
-            </li>
-          </>
-        )}
-      </ul>
-      {user && <span>Welcome {user.username}</span>}
-    </nav>
+    <header className="header">
+      <div className="header-content">
+        <h1 className="app-title"> 📝 Pro-Tasker </h1>
+        <nav className="header-nav">
+          <ul>
+            {user ? (
+              <div style={{display: 'flex', gap: '10px'}}>
+                <li >
+                  <Link style={{textDecoration: "none"}} to="/">Dashboard</Link>
+                </li>
+                <li onClick={logout}>
+                  <Link style={{textDecoration: "none"}} to="/login">Logout</Link>
+                </li>
+              </div >
+            ) : (
+              <div style={{display: 'flex', gap: '10px'}}>
+                <li >
+                  <Link style={{textDecoration: "none"}} to="/login">Login</Link>
+                </li>
+                <li>
+                  <Link style={{textDecoration: "none"}} to="/register">Register</Link>
+                </li>
+              </div >
+            )}
+          </ul>
+          {user && <span>Welcome {user.username}</span>}
+        </nav>
+      </div>
+    </header>
   );
 }
 
